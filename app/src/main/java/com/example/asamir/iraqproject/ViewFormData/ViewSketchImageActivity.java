7a55498439b8afe55c8db6acbbeb08053ed4841b
AlaaMaher <loaloa.maher@gmail.com>
2018-12-23 16:36:43 +0200

    Fixing Sketch Image

--- app/src/main/java/com/example/asamir/iraqproject/ViewFormData/ViewSketchImageActivity.java
+++ app/src/main/java/com/example/asamir/iraqproject/ViewFormData/ViewSketchImageActivity.java
@@ -1,10 +1,18 @@
 package com.example.asamir.iraqproject.ViewFormData;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.arch.persistence.room.Room;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,27 +20,50 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.asamir.iraqproject.AddFormData.SketchPlace;
 import com.example.asamir.iraqproject.AddFormData.SurvayScreen;
+import com.example.asamir.iraqproject.ConstMethods;
 import com.example.asamir.iraqproject.LoginActivity;
 import com.example.asamir.iraqproject.Models.DataCollectionModel;
+import com.example.asamir.iraqproject.Models.SketchModel;
 import com.example.asamir.iraqproject.OfflineWork.Database;
 import com.example.asamir.iraqproject.ProjectsActivity;
 import com.example.asamir.iraqproject.R;
 import com.example.asamir.iraqproject.RegistedList;
+import com.example.asamir.iraqproject.util.ConnectivityHelper;
+import com.example.asamir.iraqproject.util.Utility;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -54,6 +85,8 @@ public class ViewSketchImageActivity extends AppCompatActivity implements Naviga
     ImageView ivPicImage;
     @BindView(R.id.btn_next)
     Button btn_next;
+    @BindView(R.id.btn_delete_photo)
+    Button btnDeletePhoto;
 
 
     private String userChoosenTask;
@@ -100,6 +133,16 @@ public class ViewSketchImageActivity extends AppCompatActivity implements Naviga
 
             }
         });
+
+        ivPicImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+        storageReference = FirebaseStorage.getInstance().getReference();
+        // Assign FirebaseDatabase instance with root database name.
+        databaseReference = FirebaseDatabase.getInstance().getReference(Database_Path);
 
 
     }
@@ -161,6 +204,202 @@ public class ViewSketchImageActivity extends AppCompatActivity implements Naviga
         finish();
         Toast.makeText(getApplicationContext(), "تم تسجيل الخروج بنجاح", Toast.LENGTH_LONG).show();
     }
+
+    private void selectImage() {
+        final CharSequence[] items = {"التقاط صورة", "اختيار من الاستديو",
+                "إلغاء"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setTitle("إضافة صورة رسم كروكي للمكان");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result = Utility.checkPermission(getApplicationContext());
+
+                if (items[item].equals("التقاط صورة")) {
+                    userChoosenTask = "Take Photo";
+                    if (result)
+                        cameraIntent();
+
+                } else if (items[item].equals("اختيار من الاستديو")) {
+                    userChoosenTask = "Choose from Library";
+                    if (result)
+                        galleryIntent();
+
+                } else if (items[item].equals("إلغاء")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void galleryIntent() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+    }
+
+    private void cameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+
+    private void uploadImage(Uri tempUri) {
+        if (tempUri != null) {
+
+            if (ConnectivityHelper.isConnectedToNetwork(ViewSketchImageActivity.this)) {
+                final ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.setTitle("برجاء الانتظار جاري رفع الصورة ... ");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                final StorageReference ref = storageReference.child(Storage_Path + System.currentTimeMillis() + "." + GetFileExtension(tempUri));
+                UploadTask uploadTask = ref.putFile(tempUri);
+                Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+
+                        return ref.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()) {
+                            Uri downloadUri = task.getResult();
+                            progressDialog.dismiss();
+                            // Continue with the task to get the download URL
+                            imageUrl = String.valueOf(downloadUri);
+                            ImageUploadId = databaseReference.push().getKey();
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm aa");
+                            currentDateandTime = sdf.format(new Date());
+                            SketchModel imageUploadInfo = new SketchModel(ImageUploadId, currentDateandTime, currentDateandTime, imageUrl);
+                            Log.e("Image Url--->", imageUrl);
+                            // Adding image upload id s child element into databaseReference.
+                            databaseReference.child(ImageUploadId).setValue(imageUploadInfo);
+                            Toast.makeText(getApplicationContext(), "تم رفع الصوره بنجاح اضغط التالي لاستكمال المسح الميداني ", Toast.LENGTH_SHORT).show();
+
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), "حدث خطأ برجاء المحاولة مرة أخري ", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        // Getting image upload ID.
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        // Toast.makeText(UploadWallpaper.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else {
+
+                imageUrl = tempUri.toString();
+                ImageUploadId = "";
+
+            }
+
+        }
+    }
+
+    // Creating Method to get the selected image file Extension from File Path URI.
+    public String GetFileExtension(Uri uri) {
+
+        ContentResolver contentResolver = getApplicationContext().getContentResolver();
+
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+
+        // Returning the file Extension.
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE) {
+                picUri = onSelectFromGalleryResult(data);
+                ivPicImage.setImageURI(picUri);
+                btnDeletePhoto.setVisibility(View.VISIBLE);
+                uploadImage(picUri);
+
+            } else if (requestCode == REQUEST_CAMERA) {
+                picUri = onCaptureImageResult(data);
+                ivPicImage.setImageURI(picUri);
+                btnDeletePhoto.setVisibility(View.VISIBLE);
+                uploadImage(picUri);
+            }
+        }
+
+    }
+
+    private Uri onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        picUri = getImageUri(getApplicationContext(), thumbnail);
+        //ivImage.setImageBitmap(thumbnail);
+        return picUri;
+    }
+
+    @SuppressWarnings("deprecation")
+    private Uri onSelectFromGalleryResult(Intent data) {
+
+        Bitmap bm = null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        picUri = getImageUri(getApplicationContext(), bm);
+        return picUri;
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public void deleteImage(View view) {
+        ConstMethods.saveSketch(ViewSketchImageActivity.this, "");
+        ivPicImage.setImageDrawable(getResources().getDrawable(R.drawable.blueprint));
+        databaseReference.child(ImageUploadId).removeValue();
+
+    }
+
 
     public void closeScreen(View view) {
         finish();
